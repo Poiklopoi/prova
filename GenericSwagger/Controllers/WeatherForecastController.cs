fe363f2718984dab85aaf7dffe43f6336eea806f
Dani93plc <danilopaolucci93@gmail.com>
2022-11-15 16:08:04 +0100

    API ROTTA

--- GenericSwagger/Controllers/WeatherForecastController.cs
+++ GenericSwagger/Controllers/WeatherForecastController.cs
@@ -6,12 +6,12 @@ namespace GenericSwagger.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        private static readonly string[] Summaries, = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<WeatherForecastController>; _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
