7ab372d8664bd49f2aa0b8952949dc862d15a456
Dani93plc <danilopaolucci93@gmail.com>
2022-11-16 11:29:54 +0100

    modifica

--- GenericSwagger/Controllers/WeatherForecastController.cs
+++ GenericSwagger/Controllers/WeatherForecastController.cs
@@ -2,16 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace GenericSwagger.Controllers
 {
+    //Cambiamento
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries, = new[]
+        private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController>; _logger;
+        private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
